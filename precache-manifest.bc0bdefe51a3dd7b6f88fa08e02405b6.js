86fd304b57b632676a4eecd4cc6c82e424efa358
Li Wei <liwsuper7@gmail.com>
2019-04-01 14:52:20 +1100

    Updates

---
+++ precache-manifest.bc0bdefe51a3dd7b6f88fa08e02405b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "bf4e7e9a5104f57714aa",
+    "url": "/contactmanager/static/js/main.05156039.chunk.js"
+  },
+  {
+    "revision": "489abb5c801b4b030b13",
+    "url": "/contactmanager/static/js/2.97416df2.chunk.js"
+  },
+  {
+    "revision": "bf4e7e9a5104f57714aa",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "489abb5c801b4b030b13",
+    "url": "/contactmanager/static/css/2.b04a6a29.chunk.css"
+  },
+  {
+    "revision": "5ac5974e6b856d4a9c33fbea8fb1b953",
+    "url": "/contactmanager/index.html"
+  }
+];
